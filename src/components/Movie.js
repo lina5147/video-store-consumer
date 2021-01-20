0532ac600195237edb2137610045a80c070acfbc
Lina Do <l.do5147@gmail.com>
2021-01-19 23:52:01 -0800

    styled library page

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,12 +1,14 @@
 import React from 'react';
-
+import './Movie.css';
 
 const Movie = (props) => {
   return (
-    <div>
-      <h4>{props.title}</h4>
-      <img onClick={() => {props.onSelectedMovie(props.id)}} src={props.image_url} alt='Movie poster'/>
-    </div>
+    <div className='movie'>
+      <div className='movie_content'>
+        <h4 className='movie_content-text'>{props.title}</h4>
+        <img onClick={() => {props.onSelectedMovie(props.id)}} src={props.image_url} alt='Movie poster'/>
+      </div> 
+   </div>
     
   )
 }
