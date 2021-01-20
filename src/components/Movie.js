64dec5d88e055bc7b56a4d1fffd65be718b627cd
Lina Do <l.do5147@gmail.com>
2021-01-20 10:17:41 -0800

    changed the on click event to div in movie component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,10 +3,10 @@ import './Movie.css';
 
 const Movie = (props) => {
   return (
-    <div className='movie'>
+    <div onClick={() => {props.onSelectedMovie(props.id)}} className='movie'>
       <div className='movie_content'>
         <h4 className='movie_content-text'>{props.title}</h4>
-        <img onClick={() => {props.onSelectedMovie(props.id)}} src={props.image_url} alt='Movie poster'/>
+        <img  src={props.image_url} alt='Movie poster'/>
       </div> 
    </div>
     
