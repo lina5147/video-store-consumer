062bc329b7b6d0d437e31e9cf8919a996bd5fe31
Emily Nagai <emnagai89@gmail.com>
2021-01-21 14:08:45 -0800

    aligned customer names to center and applied color

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Customers.css';
+
 
 const Customer = (props) => {
     return (
         <div className="customer">
             {/* <h4>{props.name}</h4> */}
-            <h2 onClick={() => {props.onSelectedCustomer(props.id)}}>{props.name}</h2>
+            <h2 onClick={() => {props.onSelectedCustomer(props.id)}} className='customers-hover'>{props.name}</h2>
         </div>
     )
 }
