a080b1e45015c7da8e351cc26afb289455647aed
Lina Do <l.do5147@gmail.com>
2021-01-21 14:58:01 -0800

    styled the selected movie and selected customer

--- src/components/CustomerDetails.js
+++ src/components/CustomerDetails.js
@@ -1,11 +1,16 @@
 import React from 'react';
-
+import './CustomerDetails.css';
 const CustomerDetails = ({customer}) => {
     return (
-        <div>
-            <h4>{customer.name}</h4>
+        <div className='customer-details'>
+            <h2>{customer.name}</h2>
+            <p>Address: {customer.address}</p>
+            <p>City: {customer.city}</p>
+            <p>State: {customer.state}</p>
+            <p>Postal Code: {customer.postal_code}</p>
+            <p>Phone Number: {customer.phone}</p>
             <p>Registered At: {customer.registered_at}</p>
-            <p>Account Credit: {customer.account_credit}</p>
+            <p>Account Credit: ${customer.account_credit}</p>
             <p>Videos Checked Out: {customer.videos_checked_out_count}</p>
         </div>
     )
--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,15 +1,19 @@
 import React from 'react';
-
+import './MovieDetails.css';
 
 const MovieDetails = ({movie}) => {
   return (
-    <div>
-      <h4>{movie.title}</h4>
-      <img src={movie.image_url} alt='Movie poster' />
-      <p>Inventory: {movie.inventory}</p>
-      <p>Release Date: {movie.release_date}</p>
-      <p>Overview: {movie.overview}</p>
+    <div className='movie-details'>
+      <div className='movie-contents'>      
+        <img src={movie.image_url} alt='Movie poster' />
+      </div>
+      <div className='movie-contents'>
+        <h2>{movie.title}</h2>
+        <p>Release Date: {movie.release_date}</p>
+        <p>Overview: {movie.overview}</p>
+      </div>
     </div>
+    
     
   )
 }
